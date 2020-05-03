75cd84f88dc9274bebfbbda208f7c0a393984fc0
harshal259 <harshal259@github.com>
2020-05-03 17:31:03 +0530

    Added Properties file

---
+++ src/main/java/PropertyReader.java
@@ -0,0 +1,28 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyReader {
+
+    Properties properties = new Properties();
+    InputStream inputStream = null;
+
+    public PropertyReader() {
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        try {
+            String path = System.getProperty("user.dir") + "/src/main/resources/testConfig.properties";
+            FileInputStream configFile = new FileInputStream(path);
+            properties.load(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
